2410891f863ca3395644f73630c29f28df86b2d3
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-13 00:14:24 -0600

    Implementación de plantilla para cliente, se mejora los márgenes de la página y se implementan los bótones especiales según el diseño.

--- resources/views/layauts/plantillacliente.blade.php
+++ resources/views/layauts/plantillacliente.blade.php
@@ -9,6 +9,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 80vh;
+            margin: 0;
+            background-color: #f8f9fa;
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            text-align: center;
+            width: 100%;
+            max-width: 400px;
+        }
         footer {
             background-color: black;
         }
@@ -97,7 +111,7 @@
 </div>
 <!-- Footer con Navbar -->
 <footer class="text-center text-lg-start fixed-bottom">
-    <div class="container p-4">
+    <div class="container">
         <nav class="navbar navbar-light">
             <div class="container-fluid justify-content-center">
                 <!-- Botón Inicio -->
