db31a2aaf86dcbd5a54f5842cfbc9e0ae77ffbac
Juan Luis Vásquez <vasquezjuanluis90@gmail.com>
2024-10-15 21:30:16 -0600

    Se agrega nueva BD

--- resources/views/Lavanderia/menuAdministrador.blade.php
+++ resources/views/Lavanderia/menuAdministrador.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Botones de Pedir en espera y Solicitar Motorista-->
         <div class="row">
-            <div class="col-12 d-flex justify-content-center">
+            <div class="col-6 d-flex justify-content-center">
                 <div class="btn-container p-1" style="background-color: rgb(217, 217, 217); border-radius: 15px; width: 100%;">
                     <form action="{{ route('pedidos') }}" method="GET">
                         @csrf
@@ -21,7 +21,23 @@
                                 <img src="https://cdn-icons-png.flaticon.com/128/4003/4003663.png" alt="Pedidos en espera" style="max-width: 100%; max-height: 100%;">
                             </div>
                             <div class="d-flex justify-content-center align-items-center" style="height: 25%;">
-                                <p class="mt-5 text-center"><strong>Pedidos en espera</strong></p>
+                                <p class="mt-2 text-center"><strong>Pedidos</strong></p>
+                            </div>
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-6 d-flex justify-content-center">
+                <div class="btn-container p-1" style="background-color: rgb(217, 217, 217); border-radius: 15px; width: 100%;">
+                    <form action="{{ route('pedidos') }}" method="GET">
+                        @csrf
+                        <button type="submit" class="btn w-100 d-flex flex-column align-items-center" style="height: 170px;">
+                            <div class="d-flex justify-content-center" style="height: 75%;">
+                                <img src="https://cdn-icons-png.flaticon.com/128/4003/4003663.png" alt="Pedidos en espera" style="max-width: 100%; max-height: 100%;">
+                            </div>
+                            <div class="d-flex justify-content-center align-items-center" style="height: 25%;">
+                                <p class="mt-2 text-center"><strong>Historial</strong></p>
                             </div>
                         </button>
                     </form>
@@ -48,7 +64,7 @@
         <div class="row">
             <div class="col-12 d-flex justify-content-center">
                 <div class="btn-container p-1" style="background-color: rgb(217, 217, 217); border-radius: 15px; width: 100%;">
-                    <a href="https://www.youtube.com/" class="btn w-100">
+                    <a href="/menuAdmin/Conta" class="btn w-100">
                         <div class="d-flex align-items-center justify-content-center">
                             <img src="https://cdn-icons-png.flaticon.com/128/11689/11689919.png" alt="Cierres Contables" style="width: 80px; height: 80px; margin-right: 10px;">
                             <h3><strong>Contabilidad</strong></h3>
