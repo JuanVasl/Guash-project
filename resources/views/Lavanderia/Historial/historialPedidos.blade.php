4261aa18e4e9e2ecda0a6d7484510cc51b4f1e4d
Juan Luis Vásquez <vasquezjuanluis90@gmail.com>
2024-10-26 23:36:02 -0600

    Cambios en la vista de Historial de Pedidos

--- resources/views/Lavanderia/Historial/historialPedidos.blade.php
+++ resources/views/Lavanderia/Historial/historialPedidos.blade.php
@@ -6,14 +6,16 @@
     <div class="col-12">
         <h3 class="text-center"><strong>Historial de Pedidos</strong></h3>
     </div>
-
-    <!-- Formulario para seleccionar la fecha y filtrar -->
-    <div class="col-12 mt-4">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            <div class="btn-container mt-4 p-3" style="background-color: rgb(217, 217, 217); border-radius: 15px; width: 100%;">
+                <!-- Formulario para seleccionar la fecha y filtrar -->
+    <div class="col-12">
         <form action="{{ route('historial.pedidos') }}" method="GET" class="row justify-content-center">
             <div class="col-12">
                 <label for="fecha" class="col-form-label"><strong>Seleccionar fecha:</strong></label>
             </div>
-            <div class="col-auto">
+            <div class="col-9">
                 <input type="date" name="fecha" id="fecha" class="form-control" value="{{ $fecha }}">
             </div>
             <div class="col-auto">
@@ -60,9 +62,12 @@
             {{ $pedidos->appends(['fecha' => $fecha])->links() }}
         </div>
     </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<a href="/menuAdmin" class="btn btn-danger">Regresar</a>
+<a href="/menuAdmin" class="btn btn-danger mt-2">Regresar</a>
 
 @endsection
 
